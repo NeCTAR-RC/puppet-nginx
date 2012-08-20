d8c096f7242be33357b4fa50dde9bc492f11cd7d
Sam Morrison <sorrison@gmail.com>
2012-08-20 14:17:48 +1000

    ssl proxy definition

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,4 +9,33 @@ class nginx {
     require => Package['nginx'],
   }
 
+  define proxy( $port, $upstreams, $ssl=false, $timeout=15 ) {
+
+    file { "/etc/nginx/sites-available/${name}.conf":
+      ensure  => present,
+      content => template("nginx/proxy.conf.erb"),
+      notify  => Service['nginx'],
+      require => Package['nginx'],
+    }
+
+    file { "/etc/nginx/sites-enabled/${name}.conf":
+      ensure => link,
+      target => "/etc/nginx/sites-available/${name}.conf",
+      notify => Service['nginx'],
+    }
+  }
+
+  file { '/etc/nginx/ssl':
+    ensure => directory,
+  }
+
+  file {'/etc/nginx/sites-enabled':
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    force   => true,
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+  }
+
 }
