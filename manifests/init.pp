34b974624502230f1b8d34d3b77fe29ae3ffa237
Sam Morrison <sorrison@gmail.com>
2012-08-20 16:27:31 +1000

    SSL certs now managed virtually

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 class nginx {
 
-  package { 'nginx':
+  package { "nginx":
     ensure => installed,
   }
 
-  service { 'nginx':
+  service { "nginx":
     ensure  => running,
-    require => Package['nginx'],
+    require => Package["nginx"],
   }
 
   define proxy( $port, $upstreams, $ssl=false, $timeout=15 ) {
@@ -14,28 +14,33 @@ class nginx {
     file { "/etc/nginx/sites-available/${name}.conf":
       ensure  => present,
       content => template("nginx/proxy.conf.erb"),
-      notify  => Service['nginx'],
-      require => Package['nginx'],
+      notify  => Service["nginx"],
+      require => Package["nginx"],
     }
 
     file { "/etc/nginx/sites-enabled/${name}.conf":
       ensure => link,
       target => "/etc/nginx/sites-available/${name}.conf",
-      notify => Service['nginx'],
+      notify => Service["nginx"],
     }
   }
 
-  file { '/etc/nginx/ssl':
-    ensure => directory,
+  file { "/etc/nginx/ssl":
+    ensure  => directory,
+    require => Package["nginx"],
   }
 
-  file {'/etc/nginx/sites-enabled':
+  File <| tag == 'sslcert' |> {
+    notify +> Service['nginx'],
+  }
+
+  file {"/etc/nginx/sites-enabled":
     ensure  => directory,
     recurse => true,
     purge   => true,
     force   => true,
-    notify  => Service['nginx'],
-    require => Package['nginx'],
+    notify  => Service["nginx"],
+    require => Package["nginx"],
   }
 
 }
